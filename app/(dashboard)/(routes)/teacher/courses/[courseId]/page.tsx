24771433b19bac5cf84ab34374daac07077baf65
duy1111 <maixuanduy0605@gmail.com>
2023-10-03 17:28:04 +0700

    set up edit course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,10 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async({
     params
@@ -51,6 +55,25 @@ const CourseIdPage = async({
                     <span className="text-sm text-slate-700" >
                         Complete all fields {completedText}
                     </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16" >
+                <div>
+                    <div className="flex items-center gap-x-2" >
+                        <IconBadge
+                            icon={LayoutDashboard}
+                        
+                        />
+                        <h2 className="text-xl" >Customize your course</h2>
+                    </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
